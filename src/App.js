271c2399a669b90cb3b924fabc05747e44be8296
Chintan Mak <chintanmakwana2011@gmail.com>
2022-02-17 11:20:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Digital Company
+    </div>
+  );
+}
+
+export default App;
